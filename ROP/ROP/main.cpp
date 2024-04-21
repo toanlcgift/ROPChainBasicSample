9a5e57119e31698e778585f4c09c5883b4c57e3d
toanlcgift <toanlcgift@gmail.com>
2024-04-21 13:19:23 +0700

    add vulnerable code

---
+++ ROP/ROP/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void rop1()
+{
+	printf("ROP 1!\n");
+}
+
+void rop2() {
+	printf("ROP 2!\n");
+}
+
+void rop3() {
+	printf("ROP 3!\n");
+}
+
+void vulnerable(char* string)
+{
+	char buffer[100];
+	strcpy(buffer, string);
+}
+
+int main(int argc, char** argv)
+{
+	printf("main\n");
+	if (argv[1] != NULL)
+		vulnerable(argv[1]);
+	return 0;
+}
